2d774216ca680d48b538b2987f677a10f8a923a6
Untamo1 <niko.lietzen@aalto.fi>
2017-06-30 17:26:46 +0300

    Testataan muutosta

--- gittest.R
+++ gittest.R
@@ -2,7 +2,7 @@ gittestfun1 <- function(X){
   n <- nrow(X)
   p <- ncol(X)
   cent <- sweep(X,2,colMeans(X),"-")
-  D <- Conj(t(cent)) %*% cent/(p-1)
+  D <- Conj(t(cent)) %*% cent/(p)
   return(D)
 }
 
